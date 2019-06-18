6818c35dfa1139c43ab07afcf742b619aa1aea02
cramirez <castuloramirez@gmail.com>
2019-06-18 12:43:11 +0200

    Adding the playlist

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -5,9 +5,19 @@ import {PlaylistComponent} from "./playlist.component";
 
 @Component({
     selector: 'my-app',
-    templateUrl:'app/ts/app.component.html'
+    templateUrl:'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
 export class AppComponent {
     mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor(){
+        this.videos = [
+            new Video(1,"Installing Django","qgGIqRFvFFk", "Installing Django"),
+            new Video(1,"How To Survive In The Wilderness","Xo_MNXJwZPk", "Bucky")
+        ]
+
+    }
 }
